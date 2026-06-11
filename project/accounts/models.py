cc22a445f7bf1f1ec57db72c96589439107960aa
Hunar <hb969@snu.edu.in>
2026-06-12 01:03:53 +0530

    Phase-2 part 1 complete

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    roll_no = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(unique=True)
