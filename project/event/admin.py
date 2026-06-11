00ab602d257a9b336e305dbf26f8c8ccc7610ceb
Hunar <hb969@snu.edu.in>
2026-06-12 02:31:34 +0530

    Phase-2 done

--- project/event/admin.py
+++ project/event/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Problem, ParticipantProgress, Submission, EventConfig
 
 # Register your models here.
+admin.site.register(Problem)
+admin.site.register(ParticipantProgress)
+admin.site.register(Submission)
+admin.site.register(EventConfig)
--- project/event/models.py
+++ project/event/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from accounts.models import User
+
+PATTERN_CHOICES = [
+    (1, 'Hashmaps'),
+    (2, 'Binary Search'),
+    (3, 'Recursion'),
+    (4, 'Linked Lists'),
+    (5, 'Graphs'),
+]
+DIFFICULTY_CHOICES = [
+    (1, '1'),
+    (2, '2'),
+    (3, '3'),
+    (4, '4'),
+    (5, '5'),
+]
 
 # Create your models here.
+class Problem(models.Model):
+    title = models.CharField(max_length=200)
+    category = models.IntegerField(choices=PATTERN_CHOICES)
+    difficulty = models.IntegerField(choices=DIFFICULTY_CHOICES)
+    description = models.TextField()
+    expected_answer = models.TextField()
+
+class ParticipantProgress(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE) 
+    problem=models.ForeignKey(Problem, on_delete=models.CASCADE)
+    user_level=models.IntegerField()
+    status=models.BooleanField(default=False)
+    completed_at = models.DateTimeField(null=True, blank=True)
+
+class Submission(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE) 
+    problem=models.ForeignKey(Problem, on_delete=models.CASCADE)
+    submitted_answer = models.TextField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
+    iscorrect=models.BooleanField(default=False)
+    
+class EventConfig(models.Model):
+    started_at=models.DateTimeField()
+    ending_time=models.DateTimeField()
